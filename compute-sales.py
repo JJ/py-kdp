f034f5ee17c88c0e9f4c9848be7e1874c8375e9a
JJ Merelo <jjmerelo@gmail.com>
2019-01-15 19:11:57 +0100

    First working title

--- compute-sales.py
+++ compute-sales.py
@@ -8,8 +8,34 @@ dir = "."
 if len(sys.argv) == 2:
     dir = sys.argv[1]
 
+unit_sales={}
+title = {}
 for filename in os.listdir(dir):
     if filename.startswith("KDP ") or filename.startswith("Sales "):
-        print( filename )
-        sheet = p.get_sheet(file_name=dir+filename)
-        print(sheet)
+        records = p.get_records(file_name=dir+filename)
+        for record in records:
+            if (record['Unidades netas vendidas'] == '' ):
+                continue
+            if 'ASIN/ISBN' in record:
+                asin = 'ASIN/ISBN'
+            else:
+                asin = 'ASIN'
+            fecha = record['Fecha de las regalías']
+            if not fecha in unit_sales :
+                 unit_sales[fecha] = {}
+            unit_sales[fecha][record[asin]]=record['Unidades netas vendidas']
+            title[record[asin]] = record['Título']
+
+
+asin_sales={}
+for fecha in unit_sales:
+    for asin in unit_sales[fecha]:
+        if asin in asin_sales:
+            asin_sales[asin] = asin_sales[asin]+unit_sales[fecha][asin]
+        else:
+            asin_sales[asin] = unit_sales[fecha][asin]
+
+print("Title, Units")
+for asin in asin_sales:
+    print(title[asin], ", ", asin_sales[asin])
+    
