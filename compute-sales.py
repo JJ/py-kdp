b77a4f79dbeadf4e36601b00c37ce95d52d1fb61
JJ Merelo <jjmerelo@gmail.com>
2019-01-15 18:20:27 +0100

    Adds PoC

---
+++ compute-sales.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+import pyexcel as p
+
+sheet = p.get_sheet(file_name='/home/jmerelo/Documentos/KDP Sales Dashboard-1547192052341-3231e36e34fc8a56bab118b19f8c07ab.ods')
+print(sheet)
