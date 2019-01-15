7ca690a670193926293881617d2ccc04fa922f22
JJ Merelo <jjmerelo@gmail.com>
2019-01-15 18:33:53 +0100

    Reading data already

--- compute-sales.py
+++ compute-sales.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 
 import pyexcel as p
+import os
+import sys
 
-sheet = p.get_sheet(file_name='/home/jmerelo/Documentos/KDP Sales Dashboard-1547192052341-3231e36e34fc8a56bab118b19f8c07ab.ods')
-print(sheet)
+dir = "."
+if len(sys.argv) == 2:
+    dir = sys.argv[1]
+
+for filename in os.listdir(dir):
+    if filename.startswith("KDP ") or filename.startswith("Sales "):
+        print( filename )
+        sheet = p.get_sheet(file_name=dir+filename)
+        print(sheet)
